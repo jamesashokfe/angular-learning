d25e1bf42d5f4c8a26e92c67bc318a9451dc8ae1
James Ashok <jamesashok93@gmail.com>
2024-01-02 12:45:03 +0530

    Output event emitter data passing.

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { HousingLocation } from '../housing-location';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, NgOptimizedImage],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.scss'
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
+  @Output() imageClick = new EventEmitter<string>();
+
+  imageClicked() {
+    this.imageClick.emit('image clicked');
+  }
 }
