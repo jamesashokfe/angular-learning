5a5e99094197b3762c318a468e54ada38c3d209d
James Ashok <jamesashok93@gmail.com>
2024-01-02 12:43:40 +0530

    Filter housing locations.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,33 @@ import { HousingService } from '../housing.service';
   standalone: true,
   imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    } else {
+      this.filteredLocationList = this.housingLocationList.filter(
+        (housingLocation) =>
+          housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+      );
+    }
+  }
+
+  // String message from child component.
+  housingImageClick(text: string) {
+    console.log(text);
   }
 }
