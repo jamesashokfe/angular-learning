7b6e51990e3bcdb248f429598d43d20ad8002c1e
James Ashok <jamesashok93@gmail.com>
2024-01-02 13:27:31 +0530

    Forms module with two way data binding.

--- src/app/new-home/new-home.component.ts
+++ src/app/new-home/new-home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-home.component.html',
   styleUrl: './new-home.component.scss'
 })
 export class NewHomeComponent {
-
+  houseName: string = '';
 }
