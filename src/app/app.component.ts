6bb16969f5b9af20de1553d8c5f0b2717c011ed7
James Ashok <jamesashok93@gmail.com>
2024-01-10 18:32:27 +0530

    Angular spacing fixes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HomeComponent,  RouterLink, RouterOutlet],
+  imports: [CommonModule, HomeComponent, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
